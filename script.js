4046c15a9032613e1490137ddbbc89e81f6a409a
Reverdumonde <chocanxi2@gmail.com>
2025-05-08 21:07:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
-menuOpenButton.addEventListener("click",() => {
-    // Toggle mobile menu visibillity
+menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
-// Close menu when the close button is clicked
-menuCloseButton.addEventListener("click",() => menuOpenButton.click
-());
 
-// Initialize Swiper
-const swiper = new Swiper('.swiper', {
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+});
+
+// Swiper 초기화
+const swiper = new Swiper(".swiper", {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
